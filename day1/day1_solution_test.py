d952bc3fed9db9b6fa56ed9c99bd421f6563681a
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-01 12:58:41 +0100

    red: update tests for a merged pair/trio function

--- day1/day1_solution_test.py
+++ day1/day1_solution_test.py
@@ -5,29 +5,29 @@ class TestPairGivenSum(unittest.TestCase):
 
     def test_values(self):
         self.assertEqual(
-           find_pair_given_sum([1721, 979, 366, 299, 675, 1456], 2020),
+           find_combination_given_sum([1721, 979, 366, 299, 675, 1456], 2, 2020),
            (1721, 299)
         )
-        self.assertIsNone(find_pair_given_sum([1010], 2020))
-        self.assertIsNone(find_pair_given_sum([0, 0, 0], 2020))
+        self.assertIsNone(find_combination_given_sum([1010], 2, 2020))
+        self.assertIsNone(find_combination_given_sum([0, 0, 0], 2, 2020))
 
     def test_emptylist(self):
-        self.assertIsNone(find_pair_given_sum([], 2020))
+        self.assertIsNone(find_combination_given_sum([], 2, 2020))
 
 
 class TestTrioGivenSum(unittest.TestCase):
 
     def test_values(self):
         self.assertEqual(
-           find_trio_given_sum([1721, 979, 366, 299, 675, 1456], 2020),
+           find_combination_given_sum([1721, 979, 366, 299, 675, 1456], 3, 2020),
            (979, 366, 675)
         )
-        self.assertIsNone(find_trio_given_sum([1000, 20], 2020))
-        self.assertIsNone(find_trio_given_sum([0, 0, 0], 2020))
+        self.assertIsNone(find_combination_given_sum([1000, 20], 3, 2020))
+        self.assertIsNone(find_combination_given_sum([0, 0, 0], 3, 2020))
 
     def test_shortlist(self):
-        self.assertIsNone(find_trio_given_sum([], 2020))
-        self.assertIsNone(find_trio_given_sum([2020], 2020))
+        self.assertIsNone(find_combination_given_sum([], 3, 2020))
+        self.assertIsNone(find_combination_given_sum([2020], 3, 2020))
 
 if __name__ == '__main__':
     unittest.main()
