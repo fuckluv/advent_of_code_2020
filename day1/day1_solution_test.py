70e6e343865fd6090a4a42422293ffe04f2073b4
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-01 12:33:14 +0100

    Additionnal tests for 1b

--- day1/day1_solution_test.py
+++ day1/day1_solution_test.py
@@ -14,5 +14,20 @@ class TestPairGivenSum(unittest.TestCase):
     def test_emptylist(self):
         self.assertIsNone(find_pair_given_sum([], 2020))
 
+
+class TestTrioGivenSum(unittest.TestCase):
+
+    def test_values(self):
+        self.assertEqual(
+           find_trio_given_sum([1721, 979, 366, 299, 675, 1456], 2020),
+           (1, 2, 4)
+        )
+        self.assertIsNone(find_trio_given_sum([1000, 20], 2020))
+        self.assertIsNone(find_trio_given_sum([0, 0, 0], 2020))
+
+    def test_shortlist(self):
+        self.assertIsNone(find_trio_given_sum([], 2020))
+        self.assertIsNone(find_trio_given_sum([2020], 2020))
+
 if __name__ == '__main__':
     unittest.main()
