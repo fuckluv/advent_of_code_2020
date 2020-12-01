65f427a0ffb22f74c70382c8d306357061347e94
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-01 12:24:40 +0100

    red: writing tests

---
+++ day1/day1_solution_test.py
@@ -0,0 +1,18 @@
+import unittest
+from day1_solution import find_pair_given_sum
+
+class TestPairGivenSum(unittest.TestCase):
+
+    def test_values(self):
+        self.assertEqual(
+           find_pair_given_sum([1721, 979, 366, 299, 675, 1456], 2020),
+           (0, 3)
+        )
+        self.assertIsNone(find_pair_given_sum([1010], 2020))
+        self.assertIsNone(find_pair_given_sum([0, 0, 0], 2020))
+
+    def test_emptylist(self):
+        self.assertIsNone(find_pair_given_sum([], 2020))
+
+if __name__ == '__main__':
+    unittest.main()
