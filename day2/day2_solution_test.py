e88b7280122de0eba80ddaef4b86119e427a2bf3
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-02 13:01:02 +0100

    Added test for policy compliance

--- day2/day2_solution_test.py
+++ day2/day2_solution_test.py
@@ -1,25 +1,29 @@
 import unittest
 import pandas as pd
-from day2_solution import read_input
+from day2_solution import read_input, is_policy_compliant
 
-
-class TestReadInput(unittest.TestCase):
-
-    def test_read_input(self):
-        test_input = read_input("./test_input.txt")
-        expected = pd.DataFrame({
+def get_test_df():
+    return pd.DataFrame({
             "min_char": [1, 1, 2],
             "max_char": [3, 3, 9],
             "char": ["a", "b", "c"],
             "password": ["abcde", "cdefg", "ccccccccc"]
         })
+
+class TestReadInput(unittest.TestCase):
+
+    def test_read_input(self):
+        test_input = read_input("./test_input.txt")
+        expected = get_test_df()
         self.assertTrue((test_input.columns == expected.columns).all())
         self.assertTrue((test_input.values == expected.values).all())
 
 class TestInputPolicy(unittest.TestCase):
 
     def test_input_policy(self):
-        pass
+        actual = get_test_df().apply(is_policy_compliant, axis = 1).values
+        expected = [True, False, True]
+        self.assertTrue((actual == expected).all())
 
 if __name__ == '__main__':
     unittest.main()
