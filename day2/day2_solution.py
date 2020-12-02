b0e21fa2b0ba7251ceb58570373c540d408f0e2a
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-02 13:22:38 +0100

    Solution 2B

--- day2/day2_solution.py
+++ day2/day2_solution.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import operator
 
 def read_input(path):
     df_input = pd.read_csv(
@@ -13,6 +14,11 @@ def is_policy_compliantA(pw_dict):
     n = pw_dict["password"].count(pw_dict["char"])
     return n >= pw_dict["min_char"] and n <= pw_dict["max_char"]
 
+def is_policy_compliantB(pw_dict):
+    letters = [pw_dict["password"][i - 1] \
+            for i in (pw_dict["min_char"], pw_dict["max_char"])]
+    return operator.xor(*(l == pw_dict["char"] for l in letters))
+
 def print_solution(compliance, problem):
     print(
         f'Solution to problem {problem}:\n'
@@ -25,3 +31,7 @@ if __name__ == "__main__":
     input_df = read_input("input.txt")
     complianceA = input_df.apply(is_policy_compliantA, axis = 1)
     print_solution(complianceA, "2A")
+
+    print()
+    complianceB = input_df.apply(is_policy_compliantB, axis = 1)
+    print_solution(complianceB, "2B")
