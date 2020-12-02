10b4ddad363bf742c5e1284a2f35a065ec11deb9
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-02 13:01:50 +0100

    2a solution

--- day2/day2_solution.py
+++ day2/day2_solution.py
@@ -8,3 +8,25 @@ def read_input(path):
         names = ['min_char', 'max_char', 'char', 'password']
     )
     return df_input
+
+def is_policy_compliant(pw_dict):
+    n = pw_dict["password"].count(pw_dict["char"])
+    return n >= pw_dict["min_char"] and n <= pw_dict["max_char"]
+
+def print_solution(compliance, problem):
+    print(
+        f'Solution to problem {problem}:\n'
+        f'----------------------\n'
+        f'Number of compliant pw: {sum(compliance)}\n'
+        f'Not compliant pw: {len(compliance) - sum(compliance)}\n'
+        )
+
+if __name__ == "__main__":
+    input_df = read_input("input.txt")
+    compliance = input_df.apply(is_policy_compliant, axis = 1)
+    print_solution(compliance, "2A")
+
+
+
+
+
