86d6e1f8a078876b30eabc396c20b167cab29c52
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-02 13:59:44 +0100

    Documentation

--- day2/day2_solution.py
+++ day2/day2_solution.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import operator
 
 def read_input(path):
+    """
+        Reads multiline input of format "1-4 a: abcd" into a pandas.DataFrame
+    """
     df_input = pd.read_csv(
         path,
         sep = '-|\s+|:\s',
@@ -10,18 +12,28 @@ def read_input(path):
     )
     return df_input
 
-def is_policy_compliantA(pw_df):
+def is_policy_compliantA(password_df):
+    """
+        Checks for each line of the password_df DataFrame if the password is
+        compliant according to rule A
+    """
     def is_compliant_dict(x):
         n = x["password"].count(x["char"])
         return n >= x["min_char"] and n <= x["max_char"]
-    return pw_df.apply(is_compliant_dict, axis = 1)
+    return password_df.apply(is_compliant_dict, axis = 1)
 
-def is_policy_compliantB(pw_df):
+def is_policy_compliantB(password_df):
+    """
+        Checks for each line of the password_df DataFrame if the password is
+        compliant according to rule B
+    """
     def is_compliant_dict(x):
-        letters = [x["password"][i - 1] \
-            for i in (x["min_char"], x["max_char"])]
-        return operator.xor(*(l == x["char"] for l in letters))
-    return pw_df.apply(is_compliant_dict, axis = 1)
+        isEq1, isEq2 = (
+            x["password"][i - 1] == x["char"]
+            for i in (x["min_char"], x["max_char"])
+            )
+        return isEq1 ^ isEq2
+    return password_df.apply(is_compliant_dict, axis = 1)
 
 def print_solution(compliance, problem):
     print(
