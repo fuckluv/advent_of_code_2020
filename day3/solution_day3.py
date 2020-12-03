c6df916c5feed2ab9e1ee11ecebf1bc05e8c3e6e
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-03 12:47:38 +0100

    Passing tests

--- day3/solution_day3.py
+++ day3/solution_day3.py
@@ -13,6 +13,17 @@ def parse_forest(lines):
         return [int(d) for d in line.translate(tr)]
     return np.array([convert_line_to_int(l) for l in lines])
 
-def is_a_tree(forest, x, y):
-    """ Check if x, y coordinates in forest is a tree """
-    pass
+def is_a_tree(forest, coords):
+    """
+    Check if coords  coordinates (list of tuples), first element is
+    row, second is column, both 0 indexed) in forest is a tree
+    """
+    if type(coords) is not list:
+        raise ValueError
+    if len(coords) == 0:
+        raise ValueError
+    forest_width = forest.shape[1]
+    if any([not 0 <= r < forest_width for r, _ in coords]):
+        raise ValueError
+    is_a_tree = [forest[r, c % forest_width] == 1 for r, c in coords]
+    return is_a_tree
