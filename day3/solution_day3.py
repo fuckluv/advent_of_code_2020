d8f07193aa8a06387d546ba87ed77caf3c2cb16a
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-03 16:24:22 +0100

    aoc/day3 (#3)
    * red: parsing a forest
    
    * green: forest parsing
    
    * test for input_read
    
    * green: adds forest reading function
    
    * Test for "is_a_tree"
    
    * Passing tests
    
    * Solution 1A and 1B
    
    * Small refactoring
    
    * loosening expectation list -> any generator

---
+++ day3/solution_day3.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def read_forest(path):
+    """ Reads a forest map at "path" """
+    with open(path) as f:
+        forest = parse_forest(l.strip() for l in f)
+        return(forest)
+
+def parse_forest(lines):
+    """
+    Parses a forest from a list of row strings into numpy.array of 0s and 1s
+    (1 if there is a tree)
+    """
+    def convert_line_to_int(line):
+        tr = str.maketrans(".#", "01")
+        return [int(d) for d in line.translate(tr)]
+    return np.array([convert_line_to_int(l) for l in lines])
+
+def are_trees(forest, coords):
+    """
+    Check if "coords"  coordinates are trees (list of tuples, first element is
+    row, second is column, both 0 indexed) in "forest".
+    Returns a list of `bool` of same length as "coords".
+    """
+    def is_a_tree(forest, coord):
+        r, c = coord
+        (forest_depth, forest_width) = forest.shape
+        if not 0 <= r < forest_depth:
+            raise ValueError
+        return forest[r, c % forest_width]
+    trees = [is_a_tree(forest, c) for c in coords]
+    return trees
+
+def count_trees_on_way(direction, forest):
+    """ Counts trees on way with "direction" a tuple (x, y) """
+    max_steps = int(forest.shape[0] / direction[0])
+    coords = ((direction[0] * step, direction[1] * step) \
+        for step in range(max_steps))
+    return sum(are_trees(forest, coords))
+
+if __name__ == "__main__":
+    forest = read_forest("./input.txt")
+    direction = (1, 3)
+    ntrees1A = count_trees_on_way(direction, forest)
+    print(
+        f'Problem 3A:\n'
+        f'-----------\n'
+        f'Number of trees in direction {direction}: {ntrees1A}'
+        )
+
+    print()
+    possible_directions = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+    result1B = 1
+    for direction in possible_directions:
+        result1B *= count_trees_on_way(direction, forest)
+    print(
+        f'Problem 3B:\n'
+        f'-----------\n'
+        f'Solution: {result1B}'
+        )
+
---
+++ day3/test_solution_day3.py
@@ -0,0 +1,78 @@
+import unittest
+from solution_day3 import *
+
+def get_test_input_small():
+    return parse_forest([".#.","..#", "###"])
+
+def get_test_input():
+    return read_forest("./test_input.txt")
+
+class TestParseForest(unittest.TestCase):
+
+    def test_input(self):
+        input_arr = get_test_input_small()
+        expected_arr = np.array([[0, 1, 0], [0, 0 , 1], [1, 1, 1]])
+        self.assertTrue(type(input_arr) is np.ndarray)
+        self.assertTrue((input_arr == expected_arr).all())
+
+class TestReadForest(unittest.TestCase):
+
+    def test_input(self):
+        input_arr = get_test_input()
+        self.assertEqual(input_arr.shape, (11, 11))
+
+class TestIsATree(unittest.TestCase):
+
+    def test_are_trees(self):
+        input_arr = get_test_input_small()
+        self.assertEqual(are_trees(input_arr, []), [])
+        self.assertEqual(are_trees(input_arr, [(0, 0)]), [False])
+        self.assertEqual(are_trees(input_arr, [(0, 1)]), [True])
+        self.assertTrue(
+            all(are_trees(input_arr, [(2, 0), (2, 1)]))
+        )
+
+    def test_recycle_forest_left(self):
+        input_arr = get_test_input_small()
+        self.assertEqual(are_trees(input_arr, [(0, -1)]), [False])
+        self.assertEqual(are_trees(input_arr, [(0, -2)]), [True])
+        self.assertEqual(are_trees(input_arr, [(1, -4)]), [True])
+        self.assertEqual(are_trees(input_arr, [(1, -5)]), [False])
+        self.assertTrue(all(are_trees(input_arr, [(0, -2), (1, -4)])))
+
+    def test_recycle_forest_left(self):
+        input_arr = get_test_input_small()
+        self.assertEqual(are_trees(input_arr, [(0, 5)]), [False])
+        self.assertEqual(are_trees(input_arr, [(0, 4)]), [True])
+        self.assertEqual(are_trees(input_arr, [(1, 8)]), [True])
+        self.assertEqual(are_trees(input_arr, [(1, 7)]), [False])
+        self.assertTrue(all(are_trees(input_arr, [(0, 4), (1, 8)])))
+
+    def test_value(self):
+        input_arr = get_test_input_small()
+        with self.assertRaises(ValueError):
+            are_trees(input_arr, [(-1, 0)])
+        with self.assertRaises(ValueError):
+            are_trees(input_arr, [(3, 0)])
+        with self.assertRaises(ValueError):
+            are_trees(input_arr, [(0, 1), (3, 0)])
+        with self.assertRaises(ValueError):
+            are_trees(input_arr, [(-1, -1), (0, 0)])
+
+    def test_type(self):
+        input_arr = get_test_input_small()
+        with self.assertRaises(TypeError):
+            are_trees(input_arr, (-1, -1))
+
+class TestTreeCount(unittest.TestCase):
+
+    def test_count_trees(self):
+        forest = get_test_input_small()
+        self.assertEqual(count_trees_on_way((1, 1), forest), 1)
+        self.assertEqual(count_trees_on_way((1, -1), forest), 2)
+        self.assertEqual(count_trees_on_way((1, 2), forest), 2)
+        forest2 = get_test_input()
+        self.assertEqual(count_trees_on_way((1, 3), forest2), 7)
+
+if __name__ == "__main__":
+    unittest.main()
