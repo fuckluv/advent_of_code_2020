1fe219b8e308725c94f99f35275fa146ef32fd99
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-03 09:26:38 +0100

    green: forest parsing

--- day3/solution_day3.py
+++ day3/solution_day3.py
@@ -2,12 +2,15 @@ import numpy as np
 
 def read_forest(path):
     """ Reads a forest map at "path" """
-    pass()
+    pass
 
-def parse_forest(list):
+def parse_forest(lines):
     """ Parses a forest from a list of row strings """
-    pass()
+    def convert_line_to_int(line):
+        tr = str.maketrans(".#", "01")
+        return [int(d) for d in line.translate(tr)]
+    return np.array([convert_line_to_int(l) for l in lines])
 
 def is_a_tree(forest, x, y):
     """ Check if x, y coordinates in forest is a tree """
-    pass()
+    pass
