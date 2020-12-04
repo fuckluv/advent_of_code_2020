d618409fc23d3d23227d75be83669a13c4160da4
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-04 14:44:50 +0100

    green: read_pass_batch

--- day4/solution_day4.py
+++ day4/solution_day4.py
@@ -1,7 +1,12 @@
 
-def parse_pass_batch(stream):
+def parse_pass_batch(text):
+    """
+    Returns a genarator of passes read from a "text" (accepting
+    `splitlines()` method), where each pass is one string (with newlines
+    stripped, and space separated values)
+    """
     current_pass = []
-    for l in stream.splitlines():
+    for l in text.splitlines():
         if l == "":
             yield " ".join(current_pass)
             current_pass = []
@@ -11,4 +16,6 @@ def parse_pass_batch(stream):
 
 
 def read_pass_batch(path):
-    pass()
+    """Returns a generator of passes from a file located at "path" """
+    with open(path) as f:
+        return parse_pass_batch(f.read())
--- day4/test_solution_day4.py
+++ day4/test_solution_day4.py
@@ -1,5 +1,5 @@
 import unittest
-from solution_day4 import parse_pass_batch
+from solution_day4 import parse_pass_batch, read_pass_batch
 
 class TestParsePassBatch(unittest.TestCase):
 
@@ -17,6 +17,12 @@ class TestParsePassBatch(unittest.TestCase):
         ]
         actual = [l for l in parse_pass_batch(test_pass_batch)]
         self.assertEqual(actual, expected)
+
+class TestReadBatch(unittest.TestCase):
+
+    def test_read_batch(self):
+        actual = [l for l in read_pass_batch("./test_input.txt")]
+        self.assertEqual(len(actual), 251)
 
 if __name__ == "__main__":
     unittest.main()
