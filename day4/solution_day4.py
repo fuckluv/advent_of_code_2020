9ac44c4958927562719a33e3a5e101154063a724
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-04 15:00:27 +0100

    Tests + is_valid_pass function

--- day4/solution_day4.py
+++ day4/solution_day4.py
@@ -19,3 +19,10 @@ def read_pass_batch(path):
     """Returns a generator of passes from a file located at "path" """
     with open(path) as f:
         return parse_pass_batch(f.read())
+
+def is_valid_pass(passport):
+    """Checks if a passport (`string`) is valid or not"""
+    compulsary_fields = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    if all([passport.find(f + ":") >= 0 for f in compulsary_fields]): \
+        return True
+    return False
--- day4/test_solution_day4.py
+++ day4/test_solution_day4.py
@@ -1,5 +1,5 @@
 import unittest
-from solution_day4 import parse_pass_batch, read_pass_batch
+from solution_day4 import parse_pass_batch, read_pass_batch, is_valid_pass
 
 class TestParsePassBatch(unittest.TestCase):
 
@@ -23,6 +23,26 @@ class TestReadBatch(unittest.TestCase):
     def test_read_batch(self):
         actual = [l for l in read_pass_batch("./test_input.txt")]
         self.assertEqual(len(actual), 251)
+
+class TestValidPass(unittest.TestCase):
+
+    def test_valid_pass(self):
+        valid_passes = [
+                "ecl:g pid:8 eyr:2020 hcl:#ff byr:1937 iyr:2017 cid:1 hgt:18",
+                "ecl:g pid:8 eyr:2020 hcl:#ff byr:1937 iyr:2017 hgt:18"
+ ]
+        invalid_passes = [
+                "",
+                "pid:8 eyr:2020 hcl:#ff byr:1937 iyr:2017 cid:1 hgt:18",
+                "ecl:g eyr:2020 hcl:#ff byr:1937 iyr:2017 cid:1 hgt:18",
+                "ecl:g pid:8 hcl:#ff byr:1937 iyr:2017 cid:1 hgt:18",
+                "ecl:g pid:8 eyr:2020 byr:1937 cid:1 hgt:18",
+                "ecl:g pid:8 eyr:2020 hcl:#ff byr:1937 iyr:2017 cid:1"
+                ]
+        for p in valid_passes:
+            self.assertTrue(is_valid_pass(p))
+        for p in invalid_passes:
+            self.assertFalse(is_valid_pass(p))
 
 if __name__ == "__main__":
     unittest.main()
