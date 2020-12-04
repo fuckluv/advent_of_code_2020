400c53efc422ef2f0bcfd4bea1d8a8e736f57ab2
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-04 14:33:13 +0100

    Test batch parsing of passes

---
+++ day4/test_solution_day4.py
@@ -0,0 +1,23 @@
+import unittest
+from solution_day4 import parse_pass_batch
+
+class TestParsePassBatch(unittest.TestCase):
+
+    def test_parse_pass_batch(self):
+        test_pass_batch = (
+        "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd" "\n"
+        "byr:1937 iyr:2017 cid:147 hgt:183cm" "\n"
+        "\n"
+        "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884" "\n"
+        "hcl:#cfa07d byr:1929" "\n"
+        )
+        expected = [
+        "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
+        "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929"
+        ]
+        actual = [l for l in parse_pass_batch(test_pass_batch)]
+        self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
+
